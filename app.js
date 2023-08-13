69ffaaf12303d2b62939097f38a271ed1216f45b
Pius Aboyi <apeapius@gmail.com>
2023-08-13 20:46:58 +0100

    add webhook response commit

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3002;app.use(bodyParser.json());
+//const port = 3002;
+const port = process.env.PORT || 3002;
+app.use(bodyParser.json());
 
 
 app.get('/', (request, response) => {
 
     //console.log(request.body);
 
-    response.status(200).type('json').send(request.body);
+    response.status(200).type('json').send(
+
+        {
+            "is_allowed": false,
+            "reason": "some reason",
+            "title": "some title"
+          }
+    );
 });
 
 app.listen(port, () => {
