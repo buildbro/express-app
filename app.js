f310adc178506e90dcbb265dba5569f937288be3
Pius Aboyi <apeapius@gmail.com>
2023-08-13 20:53:51 +0100

    add webhook response commit

--- app.js
+++ app.js
@@ -11,11 +11,8 @@ app.get('/', (request, response) => {
     //console.log(request.body);
 
     response.status(200).type('json').send(
-
         {
-            "is_allowed": false,
-            "reason": "some reason",
-            "title": "some title"
+            "is_allowed": true
           }
     );
 });
